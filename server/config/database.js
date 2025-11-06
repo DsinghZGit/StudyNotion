e83364270a8b2ae27e32f436dd7dcbd530d532a3
Dilpreet Singh <ds20020917@gmail.com>
2025-11-06 11:31:14 +0530

    Initial commit - StudyNotion

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const { MONGODB_URL } = process.env;
+
+exports.connect = () => {
+	mongoose
+		.connect(MONGODB_URL, {
+			useNewUrlparser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`DB Connection Success`))
+		.catch((err) => {
+			process.exit(1);
+		});
+};
